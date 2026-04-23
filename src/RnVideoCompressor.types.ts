ec52bcf68641ebb211f3a77108608685a4c3857c
QiushiZhao <jerry.qiushi@gmail.com>
2026-04-23 15:44:19 +0100

    feat: define public types and error class

--- src/RnVideoCompressor.types.ts
+++ src/RnVideoCompressor.types.ts
@@ -1,19 +1,32 @@
-import type { StyleProp, ViewStyle } from 'react-native';
+export interface AutoOptions {
+  onProgress?: (progress: number) => void;
+}
 
-export type OnLoadEventPayload = {
-  url: string;
-};
+export interface CustomOptions {
+  width: number;
+  height: number;
+  videoBitrate: number;
+  audioBitrate?: number;
+  fps?: number;
+  onProgress?: (progress: number) => void;
+}
 
-export type RnVideoCompressorModuleEvents = {
-  onChange: (params: ChangeEventPayload) => void;
-};
+export interface CompressResult {
+  uri: string;
+  skipped: boolean;
+}
 
-export type ChangeEventPayload = {
-  value: string;
-};
+export type ErrorCode =
+  | 'ERR_INPUT_NOT_FOUND'
+  | 'ERR_OUTPUT_PATH_INVALID'
+  | 'ERR_UNSUPPORTED_SOURCE'
+  | 'ERR_ENCODING_FAILED';
 
-export type RnVideoCompressorViewProps = {
-  url: string;
-  onLoad: (event: { nativeEvent: OnLoadEventPayload }) => void;
-  style?: StyleProp<ViewStyle>;
-};
+export class VideoCompressorError extends Error {
+  code: ErrorCode;
+  constructor(code: ErrorCode, message: string) {
+    super(message);
+    this.name = 'VideoCompressorError';
+    this.code = code;
+  }
+}
